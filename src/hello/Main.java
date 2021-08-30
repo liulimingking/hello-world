bc56c722c28cef13432c0c99446bb3f0b62ec34b
liulimingking <915262557@qq.com>
2021-08-30 20:42:43 +0800

    1

---
+++ src/hello/Main.java
@@ -0,0 +1,13 @@
+package hello;
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args)
+    {
+        int a[][] = new int [100][1];
+        a[99][0]=1;
+        System.out.println(a[99][0]);
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt(),b=sc.nextInt();
+        System.out.println(b+c);
+    }
+}
